8d87ba827f1baaefc695e3c2a5d9f5e7b297ba54
dolphy <uwanduchibuike@gmail.com>
2023-08-01 05:59:40 +0100

    first commit

---
+++ scss/js/main.js
@@ -0,0 +1,33 @@
+function Validate(){
+
+    let mail_format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let entered_email = document.forms["myForm"]["email_field"].value;
+    let error_hint = document.querySelector('.error_hint');
+    let error_icon = document.querySelector('.error_icon');
+    
+    
+    if(entered_email.match(mail_format))
+    {
+        if(!error_hint.classList.contains('hide') && !error_icon.classList.contains('hide'))
+        {
+        error_hint.classList.add('hide');
+        error_icon.classList.add('hide');
+        }
+        alert("Validate Email Address");
+        return true;
+    }
+    else{
+        console.log('am inside here');
+        
+            if(error_hint.classList.contains('hide') && error_icon.classList.contains('hide'))
+            {
+                console.log('inside');
+                error_hint.classList.remove('hide');
+                error_icon.classList.remove('hide');
+                console.log(error_hint);
+                console.log(error_icon);
+            }
+        return false;
+    }
+
+}
